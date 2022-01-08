a4c8d70e6012bf2a6e850029c13d207a65b94469
EC2 Default User <ec2-user@ip-172-31-55-108.ec2.internal>
2022-01-08 18:23:05 +0000

    first

---
+++ janken.rb
@@ -0,0 +1,74 @@
+def janken
+  
+  puts "ジャンケン. . ."
+  puts "0(グー)1(チョキ)2(パー)3(戦わない)"
+  player_hand = gets.to_i
+  program_hand = rand(3)
+
+  if not (player_hand == 0 || player_hand == 1 || player_hand == 2 || player_hand == 3) #無効処理
+	  puts "入力された値が無効です"
+	  puts "-----------------"
+  	return true
+  end
+  
+  if player_hand == 3 #戦わない
+  	return false
+  end
+  
+    puts "ホイ!"
+    puts "-----------------"
+  
+  jankens=["グー","チョキ","パー"]
+    puts "あなた:#{jankens[player_hand]}, プログラム:#{jankens[program_hand]}"
+
+  if player_hand == program_hand
+	  puts "あいこ"
+	  puts "-----------------"
+	  return true
+	  
+  elsif (player_hand == 0 && program_hand ==1) ||(player_hand == 1 && program_hand ==2) || (player_hand == 2 && program_hand ==0)
+	  puts "あなたの勝ちです"
+    puts "-----------------"
+    case_pattern = "pattern1"
+     
+  else
+	  puts "負け"
+	  puts "-----------------"
+	  case_pattern = "pattern2"
+  end
+  
+  
+  case case_pattern #あっちむいてホイ
+    when "pattern1"
+  　　　puts "あっちむいて〜"
+  　　puts "0(上)1(下)2(左)3(右)"
+  
+  　　player_finger = gets.to_i
+  　　program_face = rand(4)
+  
+  　　
+  
+  　　puts "ホイ!"
+  　　puts "-----------------"
+  
+  　　direction=["上","下","左","右"]
+  　　puts "あなた:#{direction[player_finger]}, プログラム:#{direction[program_face]}"
+  
+  　　if 　player_finger == program_face
+        puts "あなたの勝利"
+        return false
+      else 
+        return true
+      end
+    
+      
+  end　#あっちむいてホイ
+  
+end #janken
+
+
+next_game = true
+
+while next_game
+  next_game = janken
+end
